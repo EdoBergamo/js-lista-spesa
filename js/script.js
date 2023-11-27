37db590df413311ab7e03164aa062885b2f36a24
EdoBergamo <hello@amtriix.it>
2023-11-27 06:56:19 +0000

    [UPDATE] Implemented initial logic

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let listaSpesa = ["Uova", "latte", "sarcicce", "Sarsa de soia"];
+
+let i = 0
+let lenLista = listaSpesa.length;
+
+while (i < lenLista) {
+    document.write(` ${listaSpesa[i]}`)
+    i++;
+}
